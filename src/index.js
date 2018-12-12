97ad3595766738e20c1ceacc4387f9a6525dbf27
rojrun <runroj@gmail.com>
2018-12-11 19:44:25 -0800

    react-intro - Roj Rungsisullatanont

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+function luckyNumber(){
+    return Math.floor(Math.random() * 1000) +1;
+}
+
+function greeting(user){
+    return <div className='container'>
+            <h1>Hello {user.name}</h1>
+            <h2 className='text-muted'>Your lucky number is : {user.luckyNumber} </h2>
+        </div>
+}
+
+const user ={
+    name: 'roj',
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+greeting(user),
+document.getElementById('root')
 );
