e34a9aead55fc00dd1edf0a349348175af504d0b
rojrun <runroj@gmail.com>
2018-12-13 09:47:14 -0800

    functional-components - Roj Rungsisullatanont

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
 
+import Table from './table';
+
+export default () => {
+
+    const students = [
+        {
+            name: 'Peter Parker',
+            course: 'Intro to Photography',
+            grade: 85
+        },
+        {
+            name: 'Bruce Banner',
+            course: 'Pshychology',
+            grade: 91
+        },
+        {
+            name: 'Tony Stark',
+            course: 'Mechanical Engineering',
+            grade: 100
+        }
+    ];
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data = {students} />
+        </div>
+    )
+}
 
