bcf0d89087e5b5d0fc4de58abcd636d2c59570e6
rojrun <runroj@gmail.com>
2018-12-20 20:01:38 -0800

    added kenny

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import Scrapbook from './scrapbook';
+import './app.css';
 
+const App = () => (
 
+            <div className="main-container">
+                <h1 className="main-title">Scrapbook</h1>
+                <Scrapbook/>
+            </div>
+);
+
+export default App;
---
+++ src/components/scrapbook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scrapbook.css';
+import kenny from './images/kenny.jpg';
+
+const Scrapbook = () => (
+    <div className="scrapbook-container">
+        <img src={kenny}/>
+    </div>
+
+);
+
+export default Scrapbook;
